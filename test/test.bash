7489e5f1410f64ed69f6d6a57eb1e696e1e99098
Kai Keiyama <s24c1049ex@s.chibakoudai.jp>
2025-12-29 15:15:27 +0900

    add test

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,9 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
-timeout 19 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+source /opt/ros/jazzy/setup.bash
+source $dir/ros2_ws/install/setup.bash
 
-cat /tmp/mypkg.log | grep 'Count:8' | grep 'Number:19'
+timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log | grep 'Count:8 | Number:19'
